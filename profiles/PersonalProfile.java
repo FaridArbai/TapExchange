b907e9e7a179c7951f9df0be1b1abec847ec104a
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-06-24 23:13:38 +0200

    Implemented background image processing

--- profiles/PersonalProfile.java
+++ profiles/PersonalProfile.java
@@ -2,49 +2,38 @@ package com.faridarbai.tapexchange.profiles;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
-import android.drm.DrmStore;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.AsyncTask;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.faridarbai.tapexchange.R;
 import com.faridarbai.tapexchange.graphical.PersonalTitlesController;
 import com.faridarbai.tapexchange.serialization.PersonData;
 import com.faridarbai.tapexchange.users.Person;
-import com.faridarbai.tapexchange.users.User;
-import com.faridarbai.tapexchange.serialization.UserData;
 
 import java.io.File;
-import java.io.IOException;
 
 public class PersonalProfile extends AppCompatActivity {
 	protected static final String TAG = "PersonalProfile";
@@ -65,13 +54,14 @@ public class PersonalProfile extends AppCompatActivity {
 		initDataView();
 		
 		adjustAvatarHeight();
+		this.updateBackground();
 		this.updateAvatar();
 	}
 	
 	private void initTitlesController(){
-		LinearLayout status = findViewById(R.id.personal_status);
-		TextView title = findViewById(R.id.personal_title);
-		AppBarLayout appbar = findViewById(R.id.personal_appbar);
+		LinearLayout status = (LinearLayout) findViewById(R.id.personal_status);
+		TextView title = (TextView) findViewById(R.id.personal_title);
+		AppBarLayout appbar = (AppBarLayout) findViewById(R.id.personal_appbar);
 		
 		this.titles_controller = new PersonalTitlesController(status, title);
 		appbar.addOnOffsetChangedListener(titles_controller);
@@ -98,41 +88,93 @@ public class PersonalProfile extends AppCompatActivity {
 		ImageView personal_image = (ImageView)findViewById(R.id.personal_image_foreground);
 		String image_path = this.user.getImagePath();
 		Bitmap bitmap = BitmapFactory.decodeFile(image_path);
+		
 		personal_image.setImageBitmap(bitmap);
+	}
+	
+	protected void updateBackground(){
+		Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.profile_background);
+		ImageView background = findViewById(R.id.personal_image_background);
 		
-		Palette palette = (new Palette.Builder(bitmap)).generate();
+		background.setImageBitmap(bitmap);
 		
-		int muted = palette.getMutedColor(0);
+		new ImageProcessingTask(bitmap).execute();
+	}
+	
+	
+	 private class ImageProcessingTask extends AsyncTask<Void, Void, Void>{
+		Bitmap bitmap;
+		int status_color, action_color, buttons_color;
 		
-		int dark_muted = palette.getDarkMutedColor(muted);
-		int dominant = palette.getDominantColor(0);
+		public ImageProcessingTask(Bitmap bitmap){
+			this.bitmap = bitmap;
+		}
 		
-		int status_color = palette.getDarkVibrantColor(dark_muted);
-		int action_color = palette.getVibrantColor(dominant);
+		 @Override
+		 protected Void doInBackground(Void... voids) {
+			Palette palette = (new Palette.Builder(bitmap)).generate();
 		
-		int buttons_color = palette.getLightVibrantColor(palette.getLightMutedColor(dominant));
+			final RenderScript rs = RenderScript.create( PersonalProfile.this );
+			final Allocation input = Allocation.createFromBitmap( rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT );
+			final Allocation output = Allocation.createTyped( rs, input.getType() );
+			final ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create( rs, Element.U8_4( rs ) );
+			script.setRadius(11.f);
+			script.setInput( input );
+			script.forEach( output );
+			output.copyTo( bitmap );
+			
+			int muted = palette.getMutedColor(0);
 		
-		this.setStatusColor(status_color);
-		this.setActionColor(action_color);
+			int dark_muted = palette.getDarkMutedColor(muted);
+			int dominant = palette.getDominantColor(0);
 		
-		FloatingActionButton bottom_button = findViewById(R.id.section_selection_button);
+			int dark_dominant = PersonalProfile.this.darken(dominant);
 		
-		bottom_button.setBackgroundTintList(ColorStateList.valueOf(buttons_color));
+			this.status_color = dark_dominant;
+			this.action_color = dominant;
+			this.buttons_color = palette.getVibrantColor(palette.getLightMutedColor(dominant));
+			
+			return null;
+		 }
 		
-		//ImageView background_image = (ImageView)findViewById(R.id.personal_image_background);
-		//background_image.setColorFilter(dominant, PorterDuff.Mode.MULTIPLY);
+		 @Override
+		 protected void onPostExecute(Void aVoid) {
+			 super.onPostExecute(aVoid);
+			 ImageView background = findViewById(R.id.personal_image_background);
+			 
+			 background.setImageBitmap(bitmap);
+			 PersonalProfile.this.setStatusColor(status_color);
+			 PersonalProfile.this.setActionColor(action_color);
+			 PersonalProfile.this.setFloatingButtonColor(buttons_color);
+		 }
+	 }
+	
+	protected void setFloatingButtonColor(int color){
+		FloatingActionButton bottom_button = (FloatingActionButton) findViewById(R.id.section_selection_button);
+		bottom_button.setBackgroundTintList(ColorStateList.valueOf(color));
+	}
+	
+	protected int darken(int color){
+		float factor = 0.8f;
+		int a = Color.alpha(color);
+		int r = Math.round(Color.red(color) * factor);
+		int g = Math.round(Color.green(color) * factor);
+		int b = Math.round(Color.blue(color) * factor);
+		int dark_color = Color.argb(a, r, g, b);
+		
+		return dark_color;
 	}
 	
 	
 	protected void adjustAvatarHeight(){
-		AppBarLayout personal_appbar = findViewById(R.id.personal_appbar);
+		AppBarLayout personal_appbar = (AppBarLayout) findViewById(R.id.personal_appbar);
 		float screen_width = getResources().getDisplayMetrics().widthPixels;
 		CoordinatorLayout.LayoutParams appbar_params = (CoordinatorLayout.LayoutParams)personal_appbar.getLayoutParams();
 		//appbar_params.height = (int)screen_width;
 	}
 	
 	public void initDataView(){
-		RecyclerView sections_view = findViewById(R.id.personal_sections_view);
+		RecyclerView sections_view = (RecyclerView) findViewById(R.id.personal_sections_view);
 		
 		sections_view.setAdapter(this.user.getSections());
 		sections_view.setLayoutManager(new LinearLayoutManager(this));
