e35cfe64fd613907268b2713a14b4e457c15e906
Farid Arbai <35288820+FaridArbai@users.noreply.github.com>
2018-07-03 23:51:36 +0200

    Implemented progress dialogs

--- MeetingActivity.java
+++ MeetingActivity.java
@@ -2,6 +2,7 @@ package com.faridarbai.tapexchange;
 
 import android.Manifest;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -11,6 +12,10 @@ import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
+import android.nfc.tech.NfcF;
+import android.nfc.tech.NfcV;
 import android.os.Build;
 import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
@@ -35,19 +40,11 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 	private static final int DISCOVERABLE_REQUEST_CODE = 1014;
 	private static final String[] RUNTIME_PERMISSIONS = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 	
-	private static boolean called_from_main;
-	private static Intent last_main_intent;
-	
-	public static void setCalledFromMain(boolean called_from_main){
-		MeetingActivity.called_from_main = called_from_main;
-	}
-	
-	private static void setLastMainIntent(Intent intent){
-		MeetingActivity.last_main_intent = intent;
-	}
-	
 	private BluetoothServer server;
 	private BluetoothClient client;
+	
+	private ProgressDialog progress_dialog;
+	
 	private byte[] user_payload;
 	
 	private User user;
@@ -56,6 +53,8 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 	private PendingIntent pending_nfc_intent;
 	private IntentFilter[] intent_filters_array;
 	private String[][] tech_lists_array;
+	
+	private ServerDescriptor last_secret_descriptor;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,15 +65,17 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 		
 		this.initNFC();
 		this.getUserPayloadFromIntent();
+		this.requestBluetoothPermissions();
 	}
 	
 	private void initNFC(){
 		this.nfc_adapter = NfcAdapter.getDefaultAdapter(this);
 		this.nfc_adapter.setNdefPushMessageCallback(this, this);
 		
-		Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		Intent intent = new Intent(this, getClass());
+		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		this.pending_nfc_intent = PendingIntent.getActivity(this, 0, intent, 0);
-	
+		
 		IntentFilter ndef_filter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
 		
 		try{
@@ -84,16 +85,36 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 			ex.printStackTrace();
 		}
 		
-		this.intent_filters_array = new IntentFilter[]{ndef_filter,};
+		this.intent_filters_array = new IntentFilter[]{ndef_filter};
 		
-		this.tech_lists_array = new String[][]{new String[]{Ndef.class.getName()}};
+		this.tech_lists_array = new String[][]{
+				new String[]{Ndef.class.getName()},
+				new String[]{NfcA.class.getName()},
+				new String[]{NfcB.class.getName()},
+				new String[]{NfcV.class.getName()},
+				new String[]{NfcF.class.getName()}};
+		
+		this.nfc_adapter.setOnNdefPushCompleteCallback(new NfcAdapter.OnNdefPushCompleteCallback() {
+			@Override
+			public void onNdefPushComplete(NfcEvent event) {
+				MeetingActivity.this.launchServer();
+			}
+		},this);
 	}
+	
+	private void launchServer(){
+		String secret_uuid = this.last_secret_descriptor.getSecretUUID();
+		this.server = new BluetoothServer(secret_uuid, this.user_payload, this);
+		this.server.start();
+		requestDiscoverability();
+	}
+	
 	
 	@Override
 	public NdefMessage createNdefMessage(NfcEvent event){
 		int payload_length = this.user_payload.length;
-		ServerDescriptor secret_descriptor = new ServerDescriptor(payload_length);
-		byte[] payload = secret_descriptor.toByteArray();
+		this.last_secret_descriptor = new ServerDescriptor(payload_length);
+		byte[] payload = this.last_secret_descriptor.toByteArray();
 		
 		String domain = "com.faridarbai";
 		String type = "faridtype";
@@ -101,23 +122,17 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 		NdefRecord record = NdefRecord.createExternal(domain, type, payload);
 		NdefMessage message = new NdefMessage(record);
 		
-		System.out.printf("Ndef creation method was called\n");
-		
-		String secret_uuid = secret_descriptor.getSecretUUID();
-		this.server = new BluetoothServer(secret_uuid, this.user_payload, this);
-		requestDiscoverability();
-		this.server.start();
-		
 		return message;
 	}
+	
 	
 	@Override
 	public void onNewIntent(Intent intent){
 		setIntent(intent);
+		
 		String action = intent.getAction();
 		String type = intent.getType();
 		String pack = intent.getPackage();
-		
 		String log_str = String.format("RECEIVED INTENT [ACTION: %s] [TYPE: %s] [PACK: %s]", action, type, pack);
 		Log.d(TAG, log_str);
 	}
@@ -133,7 +148,7 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 		super.onResume();
 		this.nfc_adapter.enableForegroundDispatch(this, this.pending_nfc_intent, this.intent_filters_array, this.tech_lists_array);
 		
-		String TARGET_ACTION = NfcAdapter.ACTION_NDEF_DISCOVERED;
+		String TARGET_ACTION = NfcAdapter.ACTION_TECH_DISCOVERED;
 		Intent last_intent = getIntent();
 		String last_intent_action;
 		boolean actions_match;
@@ -150,8 +165,8 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 				
 				if (actions_match) {
 					Log.d(TAG, "onResume: THE ACTIONS MATCH");
-					
 					this.handleReceivedNdef(last_intent);
+					setIntent(null);
 				}
 				else{
 					Log.d(TAG, "onResume: ACTIONS MISMATCH : " + last_intent_action);
@@ -169,61 +184,45 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 		
 		this.client = new BluetoothClient(secret_descriptor, this);
 		this.client.start();
+		
+		this.progress_dialog = new ProgressDialog(this);
+		this.progress_dialog.setMessage("Scanning for new contact's device");
+		this.progress_dialog.setCanceledOnTouchOutside(false);
+		this.progress_dialog.setCancelable(false);
+		
+		this.progress_dialog.show();
 	}
 	
 	
+	public void onContactsDeviceFound(){
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				MeetingActivity.this.progress_dialog.dismiss();
+		
+				MeetingActivity.this.progress_dialog = new ProgressDialog(MeetingActivity.this);
+				MeetingActivity.this.progress_dialog.setMessage("Downloading contact information");
+				MeetingActivity.this.progress_dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				MeetingActivity.this.progress_dialog.setMax(100);
+				
+				MeetingActivity.this.progress_dialog.show();
+			}
+		});
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void incrementProgressBy(float percentage){
+		final int percentage_100 = (int)(percentage*100.0);
+		
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				MeetingActivity.this.progress_dialog.setProgress(percentage_100);
+			}
+		});
+	}
 	
 	protected void getUserPayloadFromIntent(){
-		Intent intent;
-		
-		if(MeetingActivity.called_from_main) {
-			intent = getIntent();
-			MeetingActivity.setLastMainIntent(intent);
-			MeetingActivity.setCalledFromMain(false);
-			Log.d(TAG, "getUserPayloadFromIntent: USER IS TAKEN FROM THE NEW MAIN INTENT");
-		}
-		else{
-			intent = MeetingActivity.last_main_intent;
-			Log.d(TAG, "getUserPayloadFromIntent: USER IS TAKEN FROM THE OLD MAIN INTENT");
-		}
+		Intent intent = getIntent();;
 		
 		UserData user_data = (UserData)intent.getSerializableExtra("UserData");
 		User user = new User(user_data,this);
@@ -231,6 +230,8 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 		
 		this.user_payload = pm.toByteArray();
 		this.user = user;
+		
+		Log.d(TAG, "getUserPayloadFromIntent: USER IS CREATED FROM MAIN INTENT");
 	}
 	
 	
@@ -280,11 +281,16 @@ public class MeetingActivity extends AppCompatActivity implements NfcAdapter.Cre
 	
 	public void onReceiveFinished(byte[] payload){
 		Person new_contact = ProtocolMessage.fromByteArray(payload, this);
-		
 		this.user.addContact(new_contact);
 		
-		String log_str = String.format("RECEIVE HAS FINISHED : Received username {%s}", user.getName());
-		Log.d(TAG, "onReceiveFinished: " + log_str);
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				MeetingActivity.this.progress_dialog.dismiss();
+			}
+		});
+		
+		Log.d(TAG, "onReceiveFinished: FINISHED ALL SENDING");
 	}
 	
 	private void requestDiscoverability(){
